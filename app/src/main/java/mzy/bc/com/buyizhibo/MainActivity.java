62b1872f57837d9d1424d6728d7490e42fc4228c
xiaogeyo <381837094@qq.com>
2017-04-10 13:55:06 +0800

    sss

--- app/src/main/java/mzy/bc/com/buyizhibo/MainActivity.java
+++ app/src/main/java/mzy/bc/com/buyizhibo/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toast.makeText(this,"我的天哪",Toast.LENGTH_LONG).show();
+        Toast.makeText(this,"我的滴哪",Toast.LENGTH_LONG).show();
     }
 }
